16e9f6754da9e424d6f8a9521bcb7fceb68d7c7a
Maick Souza <maick_a_s@msn.com>
2020-08-05 18:47:11 -0300

    chore(server): server ad database settings

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json);
+
+app.get('/users', (request, response) => {
+  
+  const users = [
+    { name: 'Maick', age: 34 },
+    { name: 'Enilda', age: 62 },
+    { name: 'Neuza', age: 62 },
+  ]
+
+  response.json(users);
+});
+
+app.listen(3333);
