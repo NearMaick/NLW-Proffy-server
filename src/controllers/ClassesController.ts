378bff4c9ae95bbf55311eeaa0108748c11f6880
Maick Souza <maick_a_s@msn.com>
2020-08-05 21:03:42 -0300

    chore(server): ClassesController index #2

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -14,7 +14,7 @@ async index(request: Request, response: Response) {
   const filters = request.query;
 
   const subject = filters.subject as string;
-  const week_day = filters.subject as string;
+  const week_day = filters.week_day as string;
   const time = filters.time as string;
 
   if (!filters.week_day || !filters.subject || !filters.time) {
@@ -26,6 +26,14 @@ async index(request: Request, response: Response) {
   const timeInMinutes = convertHourToMinutes(time);
 
   const classes = await db('classes')
+    .whereExists(function() {
+      this.select('class_schedule.*')
+      .from('class_schedule')
+      .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+      .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+      .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+      .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+    })
     .where('classes.subject', '=', subject)
     .join('users', 'classes.user_id', '=', 'users.id')
     .select(['classes.*', 'users.*']);
